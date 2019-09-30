64f34e81689f1023d929b61116510f20b9f043df
ank12345it <32088054+ank12345it@users.noreply.github.com>
2019-09-30 09:52:38 +0530

    Initial commit

--- src/app/user/usignup/usignup.component.ts
+++ src/app/user/usignup/usignup.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Upass } from './Upass';
+import { CommonService } from 'src/app/common.service';
+import { ReturnStatement } from '@angular/compiler';
+
 
 @Component({
   selector: 'app-usignup',
@@ -9,15 +12,27 @@ import { Upass } from './Upass';
 })
 export class UsignupComponent implements OnInit {
 
-  constructor(private http:HttpClient) {  }
-  email="";
-  pass="";
-  c_pass="";
+  constructor(private http:HttpClient,private commonService:CommonService  ) {  }
+  first;second;
   ngOnInit() {
   }
-  register(){
-    const data=new Upass(this.email,this.pass,this.c_pass);
+  
+  check(){
+    //console.log(first,second);
+     if(this.first!=this.second)
+     {
+         return false;
+     }
+     else{
+       return true;
+     }
+ }
+  register(form){
+    console.log(form.value.email,form.value.pass,form.value.c_pass);
+    const data=new Upass(form.value.email,form.value.pass,form.value.c_pass);
+    
     const c = data;
+    
     const req=this.http.post('http://localhost:8091/api/user',c).toPromise()
     .then((data:any)=>{
       console.log(data);
