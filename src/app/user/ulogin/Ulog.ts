dc67251c4a2b4ef8bfcdc76035184ac3e4f88e5a
unknown <ankitkishor789@gmail.com>
2019-10-02 23:17:24 +0530

    commit

---
+++ src/app/user/ulogin/Ulog.ts
@@ -0,0 +1,13 @@
+export class Ulog{
+    
+    email:String;
+    pass:String;
+    
+    
+    constructor(email,pass){
+        this.email=email;
+        this.pass=pass;
+    
+        
+    }
+}
--- src/app/user/ulogin/ulogin.component.ts
+++ src/app/user/ulogin/ulogin.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {User} from '../user1';
+import { Ulog } from './ulog';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-ulogin',
@@ -8,11 +10,23 @@ import {User} from '../user1';
 })
 export class UloginComponent implements OnInit {
  userList:User[]=[];
-  constructor() { }
- adduser(form){
-   console.log(form.value.name);
- }
+  constructor(private http:HttpClient) { }
   ngOnInit() {
   }
-
+ login(form){
+  const data=new Ulog(form.value.email,form.value.pass);
+    
+  const c = data;
+  
+  const req=this.http.post('http://localhost:8091/api/login',c).toPromise()
+  .then((data:any)=>{
+    console.log(data);
+    localStorage.setItem('token',data.toString());
+  },
+  error=>{}
+  )
 }
+ }
+  
+
+
